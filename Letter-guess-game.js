e8ee388d4b841c80e30d883dcc27956ea70ab8d8
Forcedsquids <tktommykline@gmail.com>
2019-02-07 19:54:27 -0500

    Updated html file with psudo code and some working code

--- Letter-guess-game.js
+++ Letter-guess-game.js
@@ -1,9 +1,5 @@
 'use strict'
 
 $(document).ready(function() {
-    $("#Test-button").click (function() {
-        $(".Link-test").html("DOES WORK PLZ??");
-        $(".Link-test").css('background', 'black');
-        $(".Link-test").css('color', 'pink');
-    });
+    
 });
